161c7b1f6255afbf657ce324973887414f66fc9e
o-korol <132794412+o-korol@users.noreply.github.com>
2024-06-27 15:11:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-import time  # Import time for explicit timing
+import time
 
 # Add the current directory to sys.path for module resolution
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
