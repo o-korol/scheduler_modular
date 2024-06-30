33345287af2916bfa4e9bf0cbe8e361268fe609c
o-korol <132794412+o-korol@users.noreply.github.com>
2024-06-29 20:28:49 -0400

    Update config.py

--- module/config.py
+++ module/config.py
@@ -1,13 +1,15 @@
 from mockup.mockup import mock_modality_preferences
 
 config = {
-    "weights": { # Specify relative weights for each factor
+    "weights": {
         "days": 1,
         "gaps": 1,
-        "modality": 10,  # Specify weight for modality score
-        "sections_per_day": 100  # Specify weight for max sections per day score
+        "modality": 10, # Keep the score high if modality is important
+        "sections_per_day": 1,
+        "consistency_start_time": 0.5,  # Weight for start time consistency.  Consider setting it lower than other weights; otherwise, consistency is effectively counted twice.
+        "consistency_end_time": 0.5  # Weight for end time consistency
     },
-    "preferred_num_days": 3, # Set preferred number of days per week on campus.  Consider setting default at 3.  User can adjust.
+    "preferred_num_days": 3,
     "day_weights": {
         0: 0,
         1: 1,
@@ -24,6 +26,7 @@ config = {
         "max_allowed_gap": 20,
         "penalty_per_hour": 2
     },
-    "preferred_max_sections_per_day": 3,  # Set preferred max number of sections per day.  Consider setting default at 3.  User can adjust.
-    "modality_preferences": mock_modality_preferences()  # Use mock modality preferences for testing
+    "preferred_max_sections_per_day": 3,
+    "modality_preferences": mock_modality_preferences(),
+    "consistency_penalty_weight": 1  # Penalty weight for consistency deviations
 }
