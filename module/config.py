74ced770a812ad2570d48eefd213eb2d75c1ac48
o-korol <132794412+o-korol@users.noreply.github.com>
2024-07-04 09:45:40 -0400

    Update config.py

--- module/config.py
+++ module/config.py
@@ -8,14 +8,14 @@ config = {
         "sections_per_day": 1,
         "consistency_start_time": 0.5,  # Weight for start time consistency. Consider setting it lower than other weights; otherwise, consistency is effectively counted twice.
         "consistency_end_time": 0.5,  # Weight for end time consistency
-        "availability": 1  # Keep this weight high if availability is NOT flexible
+        "availability": 5  # Keep this weight high if availability is NOT flexible
     },
     "preferred_num_days": 3, # Preferred number of days on campus
     "penalty_per_excess_day": 1,  # Penalty for each excess day on campus
     "gap_weights": {
         "mandatory_break_start": "12:15 PM",
         "mandatory_break_end": "1:30 PM",
-        "max_allowed_gap": 20,
+        "max_allowed_gap": 20, # In minutes # Consider renaming to include units in the name
         "penalty_per_gap_hour": 2
     },
     "preferred_max_sections_per_day": 3,
@@ -23,7 +23,7 @@ config = {
     "modality_preferences": mock_modality_preferences(),
     "consistency_penalty_weight": 1,  # Penalty weight for consistency deviations
     "availability": {
-        "time_out_of_bounds": 15,  # Time interval to check out-of-bounds in minutes
-        "penalty_per_15_min": 1  # Penalty for every 15 minutes out of bounds # Consider renaming & adjusting in _scoring_availability in scoring module
+        "time_out_of_bounds": 60,  # Time interval to check out-of-bounds, in minutes (1 hour)
+        "penalty_per_hour": 1  # Penalty for every hour out of bounds
     }
 }
