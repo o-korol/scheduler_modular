55853df3730fba80a44e8cd5bc22dc50a63087be
o-korol <132794412+o-korol@users.noreply.github.com>
2024-07-08 15:15:11 -0400

    Update plotting.py

--- module/plotting.py
+++ module/plotting.py
@@ -1,12 +1,22 @@
-import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-from .utils import time_function, parse_time
-from .scoring import _extract_meeting_days
-from datetime import datetime # Not used; superceded by parse_time
+
+from module.scoring import _extract_meeting_days
+from module.utils import time_function, parse_time
+
 
 def split_text(text, max_width):
-    """Split longer text to fit inside a course patch on the plot."""
+    """
+    Split longer text to fit inside a course patch on the plot.
+
+    Args:
+        text (str): The text to be split.
+        max_width (int): The maximum width of the text in characters.
+
+    Returns:
+        str: The split text with line breaks.
+    """
     words = text.split(', ')
     lines = []
     current_line = words[0]
@@ -19,53 +29,86 @@ def split_text(text, max_width):
     lines.append(current_line)
     return '\n'.join(lines)
 
+
 def setup_axes(ax, days):
-    """Set up the axes with grid lines and labels."""
-    ax.set_xlim(-0.5, 6.5) # Set the x-axis limits to cover 7 days of the week
-    ax.set_ylim(23, 7) # Set the y-axis limits to cover hours from 7 AM to 11 PM & invert them to make the plot look like a weekly planner page
+    """
+    Set up the axes, ticks, labels, and gridlines for the plot.
+
+    Args:
+        ax (matplotlib.axes.Axes): The axes to set up.
+        days (list): The list of day labels.
+    """
+    ax.set_xlim(-0.5, 6.5)  # Set the x-axis limits to cover 7 days of the week
+    # Set the y-axis limits to cover hours from 7 AM to 11 PM
+    # Invert them to make the plot look like a weekly planner page
+    ax.set_ylim(23, 7)
     ax.set_xticks(range(len(days)))
     ax.set_xticklabels(days)
     ax.set_yticks(range(7, 23))
     ax.set_yticklabels([f"{hour}:00" for hour in range(7, 23)])
 
     # Set up grid lines
-    # (did not use default gridlines because they go through, not around, days on the x-axis)
+    # Note:  Do not use default gridlines because they go through, not around, days on the x-axis
     for i in range(len(days)):
         ax.axvline(x=i - 0.5, color='gray', linestyle='-', zorder=1)
     for hour in range(8, 23):
         ax.axhline(y=hour, color='gray', linestyle='-', zorder=1)
 
+
 @time_function
 def plot_schedule(schedule, option_number, scores):
-    """Plot a schedule in a "weekly planner page" format.  Sections with no meeting time are plotted separately, below the weekly grid."""
+    """
+    Plot a schedule in a "weekly planner page" format.
+    Sections with no meeting time are plotted separately, below the weekly grid.
+
+    Args:
+        schedule (list): The list of sections to be plotted.
+        option_number (int): The option number for the schedule.
+        scores (dict): The scores associated with the schedule.
+
+    Returns:
+        matplotlib.figure.Figure: The figure object containing the plot.
+    """
     days = ['M', 'T', 'W', 'TH', 'F', 'S', 'SU']
-    colors = ['lightblue', 'lightcoral', 'lightgreen', 'lightsalmon', 'lightpink', 'lightseagreen', 'skyblue', 'lightgoldenrodyellow', 'lightcyan', 'lightgray']
-    course_names = sorted(set(section['Course_Name'] for section in schedule)) # Sort sections alphabetically by course name
-    color_map = {course: colors[i % len(colors)] for i, course in enumerate(course_names)} # Assign colors to courses, to maintain consistent color across different schedules
+    colors = [
+        'lightblue', 'lightcoral', 'lightgreen', 'lightsalmon', 'lightpink',
+        'lightseagreen', 'skyblue', 'lightgoldenrodyellow', 'lightcyan', 'lightgray'
+    ]
+    # Sort sections alphabetically by course name
+    course_names = sorted(set(section['Course_Name'] for section in schedule))
+    # Assign colors to courses, to maintain consistent color across different schedules
+    color_map = {course: colors[i % len(colors)] for i, course in enumerate(course_names)}
 
     fig, ax = plt.subplots(figsize=(14, 10))
     setup_axes(ax, days)
 
-    no_meeting_times_sections = [] # Create and intialize a list to hold sections with no meeting times (e.g., ONLIN sections), to plot them separately
+    # Create and intialize a list to hold sections with no meeting times
+    # (e.g., ONLIN sections), to plot them separately
+    no_meeting_times_sections = []
 
     for section in schedule:
-        # Get section duration (full semester, 1st half, 2nd half, or partial).  If the info is missing, default to 'full semester'.
+        # Get section duration (full semester, 1st half, 2nd half, or partial).
+        # If the info is missing, default to 'full semester'.
         duration = section.get('Duration', 'full semester').lower()
         # Set patch width and starting position on x axis, specified as offset from the vertical lines
+        # Specifiy default patch width and x-axis position values for default value ('full semester')
+        # Modify patch width and x-axis position for half-semester sections
         width, x_offset = {
-            '1st half': (0.5, -0.5), # Modify patch width and x-axis position for half-semester sections
+            '1st half': (0.5, -0.5),
             '2nd half': (0.5, 0),
-        }.get(duration, (1, -0.5)) # Specifiy default patch width and x-axis position values for default value ('full semester')
+        }.get(duration, (1, -0.5))
 
         start_time = parse_time(section['STime'])
         end_time = parse_time(section['ETime'])
 
-        if not start_time or not end_time: # If section has no start time or end time, add to the list of sections that have no meeting times
+        # If section has no start time or end time, add to the list of sections that have no meeting times
+        if not start_time or not end_time:
             no_meeting_times_sections.append((section, width, x_offset))
             continue
 
         meeting_days = _extract_meeting_days(section)
-        start_hour = start_time.hour + start_time.minute / 60 # convert time into a decimal for plotting
+        # Convert time into a decimal for plotting
+        start_hour = start_time.hour + start_time.minute / 60
         end_hour = end_time.hour + end_time.minute / 60
         course_name = section['Course_Name']
         color = color_map.get(course_name, 'lightgrey')
@@ -74,19 +117,45 @@ def plot_schedule(schedule, option_number, scores):
             if day not in days:
                 continue
 
-            day_index = days.index(day) # Convert days of the week to numbers for plotting (e.g., M = 0, T = 1, W = 2, etc.)
-            # Specify the size and position of the patch:
-            # (x, y) position of the upper left corner; width and length; aesthetics; front-to-back position (zorder)
-            rect = patches.Rectangle((day_index + x_offset, start_hour), width, end_hour - start_hour, linewidth=1, edgecolor='black', facecolor=color, zorder=5)
+            # Convert days of the week to numbers for plotting (e.g., M = 0, T = 1, etc.)
+            day_index = days.index(day)
+            # Specify characteristics of the patch
+            rect = patches.Rectangle(
+                (day_index + x_offset, start_hour),  # (x, y) position of the upper left corner
+                width,
+                end_hour - start_hour,  # length (y-axis dimension)
+                linewidth=1,
+                edgecolor='black',
+                facecolor=color,
+                zorder=5  # front-to-back position
+            )
             ax.add_patch(rect)
-            split_name = split_text(section['Name'], max_width=15) # Split longer names to make them fit inside patches
-            ax.text(day_index, (start_hour + end_hour) / 2, split_name, verticalalignment='center', horizontalalignment='center', zorder=6, fontsize=10)
+            # Split longer names to make them fit inside patches
+            split_name = split_text(section['Name'], max_width=15)
+            ax.text(
+                day_index,  # x-coordinate for the text position
+                (start_hour + end_hour) / 2,  # y-coordinate for the text position
+                split_name,  # text content to be displayed
+                verticalalignment='center',
+                horizontalalignment='center',
+                zorder=6,
+                fontsize=10
+            )
 
-    # Generate plot title dynamically, from the scores dictionary output by combined_score function in scoring module
-    title_text = f'Schedule Option {option_number}\n' + ', '.join([f"{key.replace('_', ' ').title()}: {value}" for key, value in scores.items()])
-    plt.title(title_text, zorder=10, fontsize=10) # Reduce font size to 10 to accomodate more scores.  Consider splitting the title text into multiple lines.
+    # Generate plot title dynamically,
+    # from the scores dictionary output by combined_score function in scoring module
+    title_text = (
+        f'Schedule Option {option_number}\n' +
+        ', '.join([
+            f"{key.replace('_', ' ').title()}: {value}"
+            for key, value in scores.items()
+        ])
+    )
+    split_title_text = split_text(title_text, max_width=150)  # Width = number of characters
+    plt.title(split_title_text, zorder=10)
 
-    if no_meeting_times_sections: # Plot sections with no meeting times at the bottom of the "weekly planner page"
+    # Plot sections with no meeting times at the bottom of the "weekly planner page"
+    if no_meeting_times_sections:
         fig.subplots_adjust(bottom=0.2)
         online_ax = fig.add_axes([0.1, 0.05, 0.8, 0.1])
         online_ax.set_xlim(-0.5, 6.5)
@@ -96,18 +165,40 @@ def plot_schedule(schedule, option_number, scores):
         for index, (section, width, x_offset) in enumerate(no_meeting_times_sections):
             course_name = section['Course_Name']
             color = color_map.get(course_name, 'lightgrey')
-            rect = patches.Rectangle((x_offset, index), width * 7, 1, linewidth=1, edgecolor='black', facecolor=color, zorder=5)
+            rect = patches.Rectangle(
+                (x_offset, index),
+                width * 7,  # width = 7 * width of 1 day
+                1,  # length
+                linewidth=1,
+                edgecolor='black',
+                facecolor=color,
+                zorder=5
+            )
             online_ax.add_patch(rect)
             split_name = split_text(section['Name'], max_width=30)
-            online_ax.text(3, index + 0.5, split_name, verticalalignment='center', horizontalalignment='center', zorder=6, fontsize=10)
-
+            online_ax.text(
+                3,  # x-coordinate for the text position
+                index + 0.5,  # y-coordinate for the text position
+                split_name,  # text content to be displayed
+                verticalalignment='center',
+                horizontalalignment='center',
+                zorder=6,
+                fontsize=10
+            )
     return fig
+
 
 @time_function
 def plot_schedules(combinations):
-    """Plot top-N schedules & save them to a pdf."""
+    """
+    Plot top-N schedules & save them to a PDF file.
+
+    Args:
+        combinations (list): A list of tuples containing schedules and their scores.
+    """
+    # Specify pdf file name
     with PdfPages('schedules.pdf') as pdf:
-        top_N = 50 # Specify the number of top schedules to graph
+        top_N = 50  # Specify the number of top schedules to graph
         for i, (combination, scores) in enumerate(combinations[:top_N], start=1):
             fig = plot_schedule(combination, i, scores)
             pdf.savefig(fig)
