b998fdb71fbfebf841c5a372a9bf45b9902c5abe
o-korol <132794412+o-korol@users.noreply.github.com>
2024-07-08 15:23:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
 import os
-import time
 
 # Add the current directory to sys.path for module resolution
 # Testing:  when this line is excluded, the script still runs fine, but that may depend on PYTHONPATH
 # sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import sqlite3
-from module.database_operations import retrieve_section_info
+import time
+
+from module.database_operations import retrieve_section_info, group_sections, sort_sections_by_enrollment, sort_courses_by_variance, sort_courses_by_section_count
 from module.scheduling_logic import generate_combinations_with_coreqs
 from module.scoring import score_combinations
 from module.plotting import plot_schedules
-from module.config import config
-from module.utils import group_sections, logger, print_summary, print_execution_summary, print_error_summary
+from module.config import config, ACTIVATE_SORT_BY_ENROLLMENT, ACTIVATE_SORT_BY_VARIANCE
+from module.utils import logger, print_summary, print_execution_summary, print_error_summary
 from mockup.mockup import mock_selected_courses, mock_modality_preferences, mock_user_availability # Mock user availability is not patched in yet
 
 def main():
@@ -38,10 +39,25 @@ def main():
         df, section_columns = retrieve_section_info(cursor, selected_courses, section_cache)
         logger.info("Section info retrieved successfully")
 
-        grouped_df = group_sections(df)
+        # Sort courses by section number (ascending order)
+        # Theoretically, it should make a difference for time conflict checking function, but in practice it does not
+        # df = sort_courses_by_section_count(df)
+
+        # Sort courses by variance in section enrollments (within a course), if the option is activated in config
+        # A light way to put a thumb on the scale in favor of sections with lower enrollment, but does not seem to make practical difference
+        if ACTIVATE_SORT_BY_VARIANCE:
+            df = sort_courses_by_variance(df)
+
+        # Sort sections by enrollment (within a course), if the option is activated in config
+        # A light way to put a thumb on the scale in favor of sections with lower enrollment
+        if ACTIVATE_SORT_BY_ENROLLMENT:
+            df = sort_sections_by_enrollment(df)
+
+        # Group sections of the same course that meet at the same time, on the same days and dates, in the same location, and have same modality
+        df = group_sections(df) # Testing:  changed argument from df to sorted_df
 
         # logger.info("Generating schedule combinations...")
-        combinations = generate_combinations_with_coreqs(cursor, grouped_df, section_cache)
+        combinations = generate_combinations_with_coreqs(cursor, df, section_cache)
         # logger.info("Schedule combinations generated successfully")
 
         # logger.info("Scoring schedule combinations...")
