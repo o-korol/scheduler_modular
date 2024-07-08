7bcc62e6452cc654dad7dac92b1e6f25adf65a52
o-korol <132794412+o-korol@users.noreply.github.com>
2024-07-08 15:14:13 -0400

    Update config.py

--- module/config.py
+++ module/config.py
@@ -1,29 +1,44 @@
 from mockup.mockup import mock_modality_preferences
 
+# Set to True to sort courses by variance in enrollment (to favor sections with lower enrollment)
+ACTIVATE_SORT_BY_VARIANCE = False
+# Set to True to sort sections of the same course by enrollment (to favor sections with lower enrollment)
+ACTIVATE_SORT_BY_ENROLLMENT = True
+
 config = {
     "weights": {
         "days": 1,
         "gaps": 1,
-        "modality": 10, # Keep the weight high if modality is important
+        "modality": 10,  # Choose high weight if modality is important
         "sections_per_day": 1,
-        "consistency_start_time": 0.5,  # Weight for start time consistency. Consider setting it lower than other weights; otherwise, consistency is effectively counted twice.
+        # Consider setting consistency weights lower than other weights;
+        # otherwise, consistency effectively is counted twice.
+        "consistency_start_time": 0.5,  # Weight for start time consistency
         "consistency_end_time": 0.5,  # Weight for end time consistency
-        "availability": 5  # Keep this weight high if availability is NOT flexible
+        "availability": 5,  # Choose high weight if availability is NOT flexible
+        # Consider keeping this weight low (keep a light thumb on the scale)
+        "enrollment_balancing":  2,
+        "location_change": 1  # Weight for the location change penalty
     },
-    "preferred_num_days": 3, # Preferred number of days on campus
+    "preferred_num_days": 3,  # Preferred number of days on campus
     "penalty_per_excess_day": 1,  # Penalty for each excess day on campus
     "gap_weights": {
         "mandatory_break_start": "12:15 PM",
         "mandatory_break_end": "1:30 PM",
-        "max_allowed_gap": 20, # In minutes # Consider renaming to include units in the name
+        "max_allowed_gap": 20,  # In minutes
         "penalty_per_gap_hour": 2
     },
     "preferred_max_sections_per_day": 3,
     "penalty_per_excess_section": 1,  # Penalty for each excessive section
-    "modality_preferences": mock_modality_preferences(),
+    "modality_preferences": mock_modality_preferences(),  # This will need move out eventually
     "consistency_penalty_weight": 1,  # Penalty weight for consistency deviations
-    "availability": {
-        "time_out_of_bounds": 60,  # Time interval to check out-of-bounds, in minutes (1 hour)
-        "penalty_per_hour": 1  # Penalty for every hour out of bounds
+    "availability_penalty_per_hour": 1,  # Penalty for every hour out of bounds
+    "enrollment_balancing_penalty_rate": 1,  # Example penalty rate for enrollment balancing
+    "location_change": {
+        "minimum_permissible_gap": 2,  # Minimum permissible gap for location change, in hours
+        # Penalty for location change if gap is shorter than minimum permissible gap
+        "unacceptable_gap_penalty": 100,
+        # Penalty for location changes if gap is longer than min permissible gap
+        "acceptable_gap_penalty": 10
     }
 }
